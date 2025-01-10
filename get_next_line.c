47c8b4549f1d71ea975c81b49949d075f130d8a9
jbarratt42 <jbarratt@student.42berlin.de>
2025-01-10 14:49:44 +0100

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarratt <jbarratt@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 14:15:28 by jbarratt          #+#    #+#             */
+/*   Updated: 2025/01/10 14:44:05 by jbarratt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+ssize_t	create_list(t_node **head, int fd)
+{
+	ssize_t	size;
+	ssize_t	size_rest;
+
+	if (!*head)
+		return (add_node(head, fd));
+	shift_left(*head, seek_nl(*head) + 1);
+	size = seek_nl(*head) + 1;
+	if (size)
+	{
+		shift_left(*head, size);
+		return (size);
+	}
+	else
+		size = (*head)->size;
+	size_rest = add_node(&(*head)->next, fd);
+	if (size_rest < 0)
+	{
+		free(*head);
+		return (size_rest);
+	}
+	return (size + size_rest);
+}
+
+t_node	*consume_list(t_node *head, char *str)
+{
+	t_node	*tail;
+	size_t	i;
+
+	i = 0;
+	while (i < head->size && head->buf[i] != '\n')
+	{
+		str[i] = head->buf[i];
+		i++;
+	}
+	if (!head->next)
+	{
+		str[i] = '\n';
+		return (head);
+	}
+	tail = consume_list(head->next, str + i);
+	free(head);
+	return (tail);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_node	*head;
+	ssize_t			size;
+	char			*str;
+
+	if (head && !head->size)
+	{
+		free(head);
+		return (NULL);
+	}
+	size = create_list(&head, fd);
+	if (size < 0)
+	{
+		if (head)
+			free(head);
+		return (NULL);
+	}
+	str = malloc(size + 1);
+	if (!str)
+		return (NULL);
+	head = consume_list(head, str);
+	str[size] = '\0';
+	return (str);
+}
---
+++ get_next_line.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarratt <jbarratt@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 14:25:46 by jbarratt          #+#    #+#             */
+/*   Updated: 2025/01/10 14:39:49 by jbarratt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# define MALLOC_ERROR -2
+# define READ_ERROR -1
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+typedef struct s_node	t_node;
+typedef struct s_node
+{
+	char	buf[BUFFER_SIZE];
+	size_t	size;
+	t_node	*next;
+}			t_node;
+char	*get_next_line(int fd);
+ssize_t	seek_nl(t_node *n);
+ssize_t	file_to_node(t_node **n, int fd);
+ssize_t	add_node(t_node **n, int fd);
+void	shift_left(t_node *n, size_t width);
+ssize_t	create_list(t_node **head, int fd);
+t_node	*consume_list(t_node *head, char *str);
+#endif
